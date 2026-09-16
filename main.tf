6b77f798684d9c4743dd6413d5aa36b7a7af6715
gabbeez <gabbeez@hotmail.co.uk>
2026-09-16 18:11:27 +0100

    deploy: trigger live AWS infrastructure provisioning loop via GitOps

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 # 1. Configure the AWS Cloud Provider
 provider "aws" {
   region = "us-east-1"
@@ -7,6 +8,7 @@ provider "aws" {
 resource "aws_instance" "gabby_server" {
   ami           = "ami-0c55b159cbfafe1f0" # Standard Ubuntu cloud image template
   instance_type = "t2.micro"
+  key_name      = "gabby-demo"            # Tells AWS to link your private door key!
 
   tags = {
     Name = "gabby-nton-infrastructure-node"
